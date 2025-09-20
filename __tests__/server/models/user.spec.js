c33b4388dc46253e22aea455b17a2dda0a317866
LucianoBuhler <lucianobuhler@gmail.com>
2025-09-20 17:19:14 -0300

    feat: Added mongoose setup and teardown using beforeAll and afetrAll; created a test case for email confirm code when creating a new user

--- __tests__/server/models/user.spec.js
+++ __tests__/server/models/user.spec.js
@@ -6,21 +6,31 @@ import mongoose from 'mongoose';
 import Bcrypt from 'bcryptjs';
 
 const mongoUri = 'mongodb://localhost:27017/lb-testing-javascript-auth-app';
+const user = {
+    name: 'Test User',
+    email: 'john.doe@test.com',  
+    password: 'plaintextpassword'
+  }
 
 describe('User Model', () => {
-  it('should hash password before saving to the DB', async () => {
+  beforeAll(async () => {
     await mongoose.connect(mongoUri, { useNewUrlParser: true})
+  })
 
-    const user = {
-      name: 'Test User',
-      email: 'john.doe@test.com',  
-      password: 'plaintextpassword'
-    }
-
+  it('should hash password before saving to the DB', async () => {
     const createdUser = await User.create(user);
 
     expect(Bcrypt.compareSync(user.password, createdUser.password)).toBe(true)
 
+  })
+
+  it('should set the email confirm code for the user before saving to the database', async () => {
+      const createdUser = await User.create(user)
+
+      expect(createdUser.emailConfirmCode).toEqual(expect.any(String))
+    })
+  
+  afterAll(async() => {
     await mongoose.connection.close()
   })
 })
