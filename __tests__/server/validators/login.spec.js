ff297b16b6dfe12ead15dbd50d20b569de22d7ae
LucianoBuhler <lucianobuhler@gmail.com>
2025-09-22 17:19:35 -0300

    feat: Added Login test with jest functions, mock and spies

---
+++ __tests__/server/validators/login.spec.js
@@ -0,0 +1,58 @@
+/**
+ * @jest-environment node
+ */
+import loginValidator from '@validators/login'
+
+class Response {
+  status(status) {
+    this.status = status
+    return this
+  } 
+
+  json(data) {
+    return data
+  }
+}
+
+describe('Login Validator', () => {
+  it('should call next if validation passes', async () => {
+    const req = {
+      body: {
+        email: 'a@a.com',
+        password: 'password'
+      }
+    }
+
+    const res = {}
+    const next = jest.fn()
+
+    await loginValidator(req, res, next)
+
+    expect(next).toHaveBeenCalled()
+  })
+
+  it('should return 422 if validation fails', async () => {
+    const req = {
+      body: {
+        password: 'password'
+      }
+    }
+
+    const next = jest.fn()
+    const res = new Response()
+
+    const statusSpy = jest.spyOn(res, 'status')
+    const jsonSpy = jest.spyOn(res, 'json')
+
+    await loginValidator(req, res, next)
+    expect(statusSpy).toHaveBeenCalledWith(422)
+    expect(jsonSpy).toHaveBeenCalledWith({
+      message: 'Validation failed.',
+      data: {
+        errors: {
+          email: 'email is a required field'
+        }
+      } 
+    })
+  })
+})
