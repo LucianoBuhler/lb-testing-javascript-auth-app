66f6ef8a9008b45901b266884748e71de2508ae8
LucianoBuhler <lucianobuhler@gmail.com>
2025-09-22 15:49:50 -0300

    feat: Added a jwtTokenGeneration test case; improvements on test code readability

--- __tests__/server/models/user.spec.js
+++ __tests__/server/models/user.spec.js
@@ -4,31 +4,44 @@
 import User from '@models/user';
 import mongoose from 'mongoose';
 import Bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from '@config';
 
 const mongoUri = 'mongodb://localhost:27017/lb-testing-javascript-auth-app';
-const user = {
+
+describe('User Model', () => {
+  const user = {
     name: 'Test User',
     email: 'john.doe@test.com',  
     password: 'plaintextpassword'
   }
 
-describe('User Model', () => {
+  let createdUser;
+
   beforeAll(async () => {
     await mongoose.connect(mongoUri, { useNewUrlParser: true})
+
+    createdUser = await User.create(user)
   })
 
   it('should hash password before saving to the DB', async () => {
-    const createdUser = await User.create(user);
-
     expect(Bcrypt.compareSync(user.password, createdUser.password)).toBe(true)
 
   })
 
   it('should set the email confirm code for the user before saving to the database', async () => {
-      const createdUser = await User.create(user)
+    expect(createdUser.emailConfirmCode).toEqual(expect.any(String))
+  })
 
-      expect(createdUser.emailConfirmCode).toEqual(expect.any(String))
+  describe('The generateToken method', () => {
+    it('should generate a JWT token for the user', () => {
+      const token = createdUser.generateToken()
+
+      const { id } = jwt.verify(token, config.jwtSecret)
+
+      expect(id).toEqual(JSON.parse(JSON.stringify(createdUser._id)))
     })
+  })
   
   afterAll(async() => {
     await mongoose.connection.close()
